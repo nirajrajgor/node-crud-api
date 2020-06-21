960f2e0a83b80c932623673caf854b1cdbfb81ee
Niraj Rajgor <nirajr1992@gmail.com>
2020-06-21 20:14:31 +0530

    Save book records implementation complete.

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI')
-const bookRouter = express.Router();
+
 const port = process.env.PORT || 4000;
 const Book = require('./models/bookModel');
 
-bookRouter.route('/books')
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err)
-        return res.send(err);
-      return res.json(books);
-    });
-  });
+const bookRouter = require('./routes/bookRouter')(Book);
 
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err)
-        return res.send(err);
-      return res.json(book);
-    });
-  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api', bookRouter);
 
