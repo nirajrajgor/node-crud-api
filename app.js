9f60fff82d60f5bc6131324d439a8f1bfdaf0d59
Niraj Rajgor <nirajr1992@gmail.com>
2020-06-21 19:41:39 +0530

    Find by id implemented.

--- app.js
+++ app.js
@@ -9,11 +9,23 @@ const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    const { query } = req;
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
     Book.find(query, (err, books) => {
       if (err)
         return res.send(err);
       return res.json(books);
+    });
+  });
+
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err)
+        return res.send(err);
+      return res.json(book);
     });
   });
 
