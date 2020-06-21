9a8ad3de4a4795c66463798cb6d2f03219db6d0a
Niraj Rajgor <nirajr1992@gmail.com>
2020-06-21 20:43:48 +0530

    Put and Patch implemented.

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -21,14 +21,47 @@ function routes(Book) {
       });
     });
 
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err)
+        return res.send(err);
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  })
+
   bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      book.save();
+      return res.json(book);
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach(item => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      })
+
+      book.save(err => {
         if (err)
           return res.send(err);
         return res.json(book);
-      });
-    });
+      })
+    })
 
   return bookRouter;
 }
